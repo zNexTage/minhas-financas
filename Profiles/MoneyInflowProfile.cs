5c26c1f84bb63ada5b8f698d0234be9dd22a6102
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-05-28 13:10:36 -0300

    Created Profile to MoneyInflow.

---
+++ Profiles/MoneyInflowProfile.cs
@@ -0,0 +1,14 @@
+using System;
+using AutoMapper;
+using MinhasFinancas.DTO.MoneyInflow;
+using MinhasFinancas.Models;
+
+namespace MinhasFinancas.Profiles;
+
+public class MoneyInflowProfile : Profile
+{
+    public MoneyInflowProfile()
+    {
+        CreateMap<CreateMoneyInflowDto, MoneyInflow>();
+    }
+}
