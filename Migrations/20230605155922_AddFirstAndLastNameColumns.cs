054ef96767303ccd5e465e1a3796e2d9121929a5
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-06-05 13:01:00 -0300

    Added First and Last name in User model.

---
+++ Migrations/20230605155922_AddFirstAndLastNameColumns.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinhasFinancas.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFirstAndLastNameColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "varchar(80)",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "varchar(80)",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -1,10 +1,20 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace MinhasFinancas.Models;
 
 public class User : IdentityUser
 {
+    [Required]
+    [MaxLength(80, ErrorMessage = "Informe o primeiro nome do usuário")]
+    public string FirstName { get; set; }
+
+    [Required]
+    [MaxLength(80, ErrorMessage = "Informe o último nome do usuário")]
+    public string LastName {get;set;}
     public virtual ICollection<MoneyOutflow> MoneyOutflows {get;set;}
     public virtual ICollection<MoneyInflow> MoneyInflows { get; set; }
+
+    public string Fullname { get => $"{FirstName} {LastName}"; }
 }
